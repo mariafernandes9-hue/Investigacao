cf47e32150474b604bfc9d1c098dff145bd7a517
mariafernandes9-hue <maria.fernandes9@estudante.ifms.edu.br>
2026-06-27 15:45:20 -0400

    correção fa tela loja

--- projeto_final/src/main/java/com/crimevariavel/model/Jogador.java
+++ projeto_final/src/main/java/com/crimevariavel/model/Jogador.java
@@ -16,20 +16,6 @@ public class Jogador {
         this.pontosUpgrade = pontosUpgrade; this.casosResolvidos = casosResolvidos;
     }
     
-    
-
-    /*public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public int getMoedas() { return moedas; }
-    public void setMoedas(int moedas) { this.moedas = moedas; }
-    public int getPontosUpgrade() { return pontosUpgrade; }
-    public void setPontosUpgrade(int p) { this.pontosUpgrade = p; }
-    public int getCasosResolvidos() { return casosResolvidos; }
-    public void setCasosResolvidos(int c) { this.casosResolvidos = c; }
-    public String toString() { return "Jogador{id=" + id + ", nome='" + nome + "'}"; }*/
-
     public int getId() {
         return id;
     }
